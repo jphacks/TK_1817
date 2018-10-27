097e2ad2b5902966cfcb92a90afce601cda6f086
furikake6000 <ewave63@keio.jp>
2018-10-27 10:02:20 +0900

    Spotify Access Set

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,56 @@
 module ApplicationHelper
+  # RbNaClを使用して対象鍵暗号を施す
+  require 'rbnacl'
+  require 'uri'
+
+  # 暗号化
+  def encrypt_data(data, password, salt)
+    key = RbNaCl::Hash.sha256(password)[0..31]
+    nonce = RbNaCl::Hash.sha256(salt)[0..23]
+    secret_box = RbNaCl::SecretBox.new(key)
+    secret_box.encrypt(nonce, data)
+  end
+
+  # 復号化
+  def decrypt_data(cipherdata, password, salt)
+    key = RbNaCl::Hash.sha256(password)[0..31]
+    nonce = RbNaCl::Hash.sha256(salt)[0..23]
+    secret_box = RbNaCl::SecretBox.new(key)
+    data = secret_box.decrypt(nonce, cipherdata)
+    # このままだとASCII-8bit形式なので、日本語用にUTF-8に変更
+    data.to_s.force_encoding('utf-8')
+  end
+
+  # cookie取得
+  def getcookie(tag)
+    cookies.signed[tag]
+  end
+
+  # cookie保存
+  def setcookie(tag, value)
+    cookies.permanent.signed[tag] = value
+  end
+
+  # cookie削除
+  def deletecookie(tag)
+    cookies.delete(tag)
+  end
+
+  # リダイレクト先を保存しておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
+  # 記憶したリダイレクト先に戻る
+  def redirect_back_or(default)
+    # Redirect to session[:forwarding_url], or default if nil
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # テストなどのためにランダム文章を生成する
+  def random_sentence
+    rand = Random.new
+    "I am #{rand(1..10000000)}! Nice to meet you!!"
+  end
 end
---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,9 @@
+require 'rspotify/oauth'
+
+# Spotify API
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :spotify,
+           Rails.application.credentials.spotify[:id],
+           Rails.application.credentials.spotify[:secret],
+           scope: "user-read-email playlist-modify-public user-library-read user-library-modify user-read-currently-playing user-modify-playback-state user-read-recently-played user-read-playback-state streaming"
+end
