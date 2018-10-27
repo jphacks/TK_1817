6ccf3a1a71685da36a2d6ab0e3625bda197d7495
furikake6000 <ewave63@keio.jp>
2018-10-28 00:19:33 +0900

    Add end(endpoint) to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_23_061833) do
+ActiveRecord::Schema.define(version: 2018_10_27_151900) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_10_23_061833) do
     t.datetime "updated_at", null: false
     t.string "spotifydata"
     t.string "email"
+    t.string "end"
   end
 
 end
