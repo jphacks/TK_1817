45ab631f7b93ea2ddf493498f8444fe248e1fcf6
furikake6000 <ewave63@keio.jp>
2018-10-27 14:44:28 +0900

    playイベントを作成 homeの修正によりデータが送れるようになる

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # User controller
   get '/status', to: 'users#status'
   get '/logout', to: 'users#logout'
+  post '/play', to: 'users#play'
 
   # Spotify API callback
   get '/auth/spotify/callback', to: 'users#spotify'
